f3e776cf6e951a8e963a28c6af9341e3d9b2d7c5
johndoe <johndoe@e395a8f9-5cf0-422e-971d-9a083d8f24e8>
2009-03-09 10:38:44 +0000

    - Added new and shiny Animation class which makes handling animations much easier; it's not yet used in the main code since it requires rewriting bigger parts of the engine - Var/func renaming
    git-svn-id: https://svn.scummvm.org:4444/svn/comet@38 e395a8f9-5cf0-422e-971d-9a083d8f24e8

---
+++ engines/comet/animation.h
@@ -0,0 +1,86 @@
+#ifndef COMET_ANIMATION_H
+#define COMET_ANIMATION_H
+
+#include "common/scummsys.h"
+#include "common/array.h"
+#include "common/util.h"
+#include "common/endian.h"
+
+#include "common/stream.h"
+
+#include "comet/comet.h"
+
+namespace Comet {
+
+struct Point {
+	int16 x, y;
+};
+
+enum AnimationCommandType {
+	kActElement			= 0,
+	kActCelSprite		= 1,
+	kActNop0            = 2,
+	kActNop1            = 3,
+	kActFilledPolygon   = 4,
+	kActRectangle       = 5,
+	kActPolygon         = 6,
+	kActPixels          = 7,
+	kActPolygon1        = 8,    // unused in Comet? / Alias for kActPolygon
+	kActPolygon2        = 9,    // unused in Comet? / Alias for kActPolygon
+	kActCelRle          = 10
+};
+
+struct AnimationCommand {
+	byte cmd;
+	//byte pointsCount -- stored in array
+	byte arg1, arg2;
+	Common::Array<Point> points;
+};
+
+struct AnimationElement {
+	byte flags;
+	//byte commandCount -- stored in array
+	Common::Array<AnimationCommand*> commands;
+	~AnimationElement();
+};
+
+struct AnimationCel {
+	byte width, height;
+	uint16 dataSize;
+	byte *data;
+};
+
+struct AnimationFrame {
+	uint16 elementIndex;
+	uint16 flags;
+	int16 xOffs, yOffs;
+};
+
+struct AnimationFrameList {
+	byte priority;
+	//byte frameCount -- stored in array
+	Common::Array<AnimationFrame*> frames;
+	~AnimationFrameList();
+};
+
+class Animation {
+public:
+    Animation();
+    ~Animation();
+    void load(Common::SeekableReadStream &sourceS, uint dataSize);
+protected:
+	typedef Common::Array<uint32> OffsetArray;
+    Common::Array<AnimationElement*> _elements;
+	Common::Array<AnimationCel*> _cels;
+	Common::Array<AnimationFrameList*> _frames;
+	// TODO: What is section 4 used for? Special palette?
+	void free();
+	void loadOffsets(Common::SeekableReadStream &sourceS, OffsetArray &offsets);
+	AnimationElement *loadAnimationElement(Common::SeekableReadStream &sourceS);
+	AnimationCommand *loadAnimationCommand(Common::SeekableReadStream &sourceS, bool ptAsByte);
+	AnimationFrameList *loadAnimationFrameList(Common::SeekableReadStream &sourceS);
+};
+
+} // End of namespace Comet
+
+#endif
