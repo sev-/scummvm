dded577a11c54a26dfdb233c2f72094c47e39804
johndoe <johndoe@1c9d93c4-8abd-43b8-a723-fa079cd96e5b>
2010-06-15 07:30:33 +0000

    Fix warning
    git-svn-id: https://svn.scummvm.org:4444/svn/prisoner@19 1c9d93c4-8abd-43b8-a723-fa079cd96e5b

--- engines/prisoner/detection.cpp
+++ engines/prisoner/detection.cpp
@@ -133,7 +133,9 @@ static const ADParams detectionParams = {
 	// Flags
 	0,
 	// Additional GUI options (for every game}
-	Common::GUIO_NONE
+	Common::GUIO_NONE,
+	// Maximum directory depth
+	1
 };
 
 class PrisonerMetaEngine : public AdvancedMetaEngine {
