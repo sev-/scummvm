f6bebd9e61c54cd7dbd8d7599e85eff588a36a39
johndoe <johndoe@e395a8f9-5cf0-422e-971d-9a083d8f24e8>
2011-01-29 02:49:11 +0000

    COMET: Replace the implode decompression code This is less code and is (imo) much easier to read/understand than the old code. Also it reads directly from the file and doesn't need a buffer to hold the compressed data. It's based on gunzip, a ZIP unpacker for the C64 (really), I put it in a class and merged/moved some code around.
    git-svn-id: https://svn.scummvm.org:4444/svn/comet@185 e395a8f9-5cf0-422e-971d-9a083d8f24e8

--- engines/comet/unpack.h
+++ engines/comet/unpack.h
@@ -1,9 +1,38 @@
-#ifndef _UNPACK_DAMIEN_H_
-#define _UNPACK_DAMIEN_H_
+#ifndef COMET_UNPACK_H
+#define COMET_UNPACK_H
 
 #include "common/util.h"
+#include "common/stream.h"
 #include "common/endian.h"
 
-int PAK_explode(uint8 *srcBuffer, uint8 *dstBuffer, uint32 compressedSize, uint32 uncompressedSize, uint16 flags);
+struct HufNode {
+	unsigned short b0;
+	unsigned short b1;
+	HufNode *jump;
+};
+
+class DecompressImplode {
+public:
+	int decompress(Common::ReadStream *source, int flags, int size, byte *dest);
+protected:
+	Common::ReadStream *_source;
+	byte *_dest;
+	byte _bitBuf;
+	int _bitBufLeft;
+	byte _buffer[32768];
+	int _bufferPos;
+	int _bytesWritten;
+	HufNode _literalTree[256];
+	HufNode _distanceTree[64];
+	HufNode _lengthTree[64];
+	byte readByte();
+	int readBit();
+	int readBits(int count);
+	void putByte(byte value);
+	void flush();
+	void recreateTree(HufNode *&currentTree, byte &len, int16 *fpos, int *flens, int16 fmax);
+	int decodeSFValue(HufNode *currentTree);
+	int createTree(HufNode *currentTree);
+};
 
 #endif
