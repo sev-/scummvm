d2709f1fc0b45bb9e422ecfbd129c6cdcea9dc26
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2023-06-04 17:29:34 +0200

    BACKENS: Fix include vs config preprocessor directive order

--- backends/printing/printman.cpp
+++ backends/printing/printman.cpp
@@ -18,10 +18,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+ 
+#include "printman.h"
 
 #ifdef USE_PRINTING
-
-#include "printman.h"
 
 PrintingManager::~PrintingManager() {}
 
