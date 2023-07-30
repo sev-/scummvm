a06172ef34cc6821f32659bb429280a88b30eb50
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2023-07-30 18:42:25 +0200

    WIN32: Correct bitCount calculation
    winapi cares about actual, not effective, number of bits per pixel

--- backends/printing/win32/win32-printman.cpp
+++ backends/printing/win32/win32-printman.cpp
@@ -397,8 +397,8 @@ HBITMAP Win32PrintJob::buildBitmap(HDC hdc, const Graphics::ManagedSurface &surf
 	bitmapInfo->bmiHeader.biWidth = surf.w;
 	bitmapInfo->bmiHeader.biHeight = -((LONG)surf.h); // Blame the OS2 team for bitmaps being upside down
 	bitmapInfo->bmiHeader.biPlanes = 1;
-	bitmapInfo->bmiHeader.biBitCount = (surf.format.isCLUT8()?8:((surf.format.bpp()+7)&~7));
 	bitmapInfo->bmiHeader.biCompression = BI_RGB;
+	bitmapInfo->bmiHeader.biBitCount = (surf.format.isCLUT8()?8:(surf.format.bytesPerPixel*8));
 	bitmapInfo->bmiHeader.biSizeImage = 0;
 	bitmapInfo->bmiHeader.biClrUsed = (surf.format.isCLUT8()?colorCount:0);
 	bitmapInfo->bmiHeader.biClrImportant = (surf.format.isCLUT8() ? colorCount : 0);
